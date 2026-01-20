1219168e646bfba0cee26d46424f0987d41088bd
Gossk <vidalmiguel535@gmail.com>
2026-01-20 15:05:14 -0500

    feat: add CustomerServiceImpl

---
+++ src/main/java/com/icodeap/crud_fullstack_angular/service/CustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package com.icodeap.crud_fullstack_angular.service;
+
+import com.icodeap.crud_fullstack_angular.entity.Customer;
+import com.icodeap.crud_fullstack_angular.repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService{
+    private CustomerRepository customerRepository;
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Customer findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return null;
+    }
+}
