125abca827edfbcca66231067322591bb37f95e3
Gossk <vidalmiguel535@gmail.com>
2026-01-20 15:08:17 -0500

    feat: add @Service annotation to CustomerServiceImpl

--- src/main/java/com/icodeap/crud_fullstack_angular/service/CustomerServiceImpl.java
+++ src/main/java/com/icodeap/crud_fullstack_angular/service/CustomerServiceImpl.java
@@ -2,9 +2,11 @@ package com.icodeap.crud_fullstack_angular.service;
 
 import com.icodeap.crud_fullstack_angular.entity.Customer;
 import com.icodeap.crud_fullstack_angular.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
     private CustomerRepository customerRepository;
     @Override
