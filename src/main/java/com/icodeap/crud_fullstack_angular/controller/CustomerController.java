208c1447277579a9c88544aca62ea007bc82fa4c
Gossk <vidalmiguel535@gmail.com>
2026-01-20 19:56:37 -0500

    feat(controller): add GET endpoint

--- src/main/java/com/icodeap/crud_fullstack_angular/controller/CustomerController.java
+++ src/main/java/com/icodeap/crud_fullstack_angular/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.icodeap.crud_fullstack_angular.controller;
 
 import com.icodeap.crud_fullstack_angular.entity.Customer;
 import com.icodeap.crud_fullstack_angular.service.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class CustomerController {
     public Customer save(@RequestBody Customer customer) {
         return customerService.save(customer);
     }
-
+    //localhost:8080
+    @GetMapping
     public List<Customer> findAll(){
         return customerService.findAll();
     }
